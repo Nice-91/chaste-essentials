1051bb4d6ccc8dcad9fc9a9a6b0f7a55a67cfddf
Nice-91 <iradukundanice91@gmail.com>
2026-01-23 06:47:07 +0200

    changing settings

--- chaste/settings.py
+++ chaste/settings.py
@@ -149,3 +149,17 @@ REST_FRAMEWORK = {
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",  # or "DEBUG" for even more info
+    },
+}
