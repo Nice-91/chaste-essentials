dc2041cb335fd08e17cd3f33d3b38855d011187e
Nice-91 <iradukundanice91@gmail.com>
2026-01-24 04:40:31 +0200

    updating serializers

--- products/serializers.py
+++ products/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from .models import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = ["id", "name", "price", "category", "description", "image"]
+        fields = ['id', 'name', 'price', 'category', 'description', 'image']
 
     def get_image(self, obj):
-        return obj.image.url if obj.image else None
+        if obj.image:
+            try:
+                # Use obj.image.url directly if available
+                return obj.image.url
+            except Exception as e:
+                print(f"Cloudinary error: {e}")
+                return None
+        return None
