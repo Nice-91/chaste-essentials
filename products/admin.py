5e42f2432afb7fd125ad56ad12ef46d9e06ffa59
Nice-91 <iradukundanice91@gmail.com>
2026-01-25 13:28:13 +0200

    update admin

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+from django import forms
+from cloudinary.forms import CloudinaryFileField
 from .models import Product
 
-admin.site.register(Product)
+
+class ProductAdminForm(forms.ModelForm):
+    image = CloudinaryFileField()
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductAdminForm
+
+
+admin.site.register(Product, ProductAdmin)
